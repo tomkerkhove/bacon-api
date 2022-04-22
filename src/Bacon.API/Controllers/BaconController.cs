54f9cdb466d60e26c371ef954d7c980fa5ac8b31
Tom Kerkhove <kerkhove.tom@gmail.com>
2022-04-22 08:46:57 +0200

    feat: Provide Bacon API
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

---
+++ src/Bacon.API/Controllers/BaconController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bacon.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/bacon")]
+    public class BaconController : ControllerBase
+    {
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<string> Get()
+        {
+            return new List<string>
+            {
+                "Honey Barbecue Flavored Bacon",
+                "Infamous Black Pepper Bacon",
+                "Italian Bacon",
+                "Raspberry Chipotle",
+                "Pumpkin Pie Spiced"
+            };
+        }
+    }
+}
