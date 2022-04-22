e34406044451e88d6e496fdfe1dcf00b6a873c7b
Tom Kerkhove <kerkhove.tom@gmail.com>
2022-04-22 08:51:58 +0200

    feat: Provide better OpenAPI docs
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- src/Bacon.API/Program.cs
+++ src/Bacon.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,19 +8,39 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Bacon API",
+        Description = "An API to use in demos to get various flavors of bacon.",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "GitHub",
+            Url = new Uri("https://github.com/tomkerkhove/bacon-api")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "MIT",
+            Url = new Uri("https://github.com/tomkerkhove/bacon-api/blob/main/LICENSE")
+        }
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Configure Swagger
+app.UseSwagger();
+app.UseSwaggerUI(o =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    o.ConfigObject.DocExpansion = DocExpansion.Full;
+    o.DocumentTitle = "Bacon API";
+});
 
+// Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
